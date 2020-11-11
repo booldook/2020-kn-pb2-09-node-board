5979822731c2c0a21dc50c1b957a8ce4a7f66a17
booldook <booldook@gmail.com>
2020-11-11 15:31:19 +0900

    멀터 작업중

--- app.js
+++ app.js
@@ -2,9 +2,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+const moment = require('moment');
+const { v4: uuidv4 } = require('uuid'); 
 
-const multer  = require('multer')
-const upload = multer({ dest: path.join(__dirname, './storage') });
+const multer  = require('multer');
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		var folder = path.join(__dirname, './storage', moment().format('YYMMDD'));
+		if(!fs.existsSync(folder)) fs.mkdirSync(folder);
+		cb(null, folder);
+	},
+	filename: (req, file, cb) => {
+		var ext = path.extname(file.originalname); //.jpg
+		var name = moment().format('YYMMDD') + '-' + uuidv4() + ext;
+		cb(null, name);
+	}
+});
+const upload = multer({ storage });
 
 /** 라우터 등록 **********************/
 const testRouter = require('./routes/test');
@@ -23,12 +38,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, './views'));
 app.locals.pretty = true;
 
-
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 /** 라우터설정 **********************/
 app.use('/', express.static(path.join(__dirname, './public')));
+app.use('/upload', express.static(path.join(__dirname, './storage')));
 app.use('/test', testRouter);
 app.use('/book', bookRouter);
 
