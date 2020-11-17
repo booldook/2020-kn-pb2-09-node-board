cc84731c33bbdac8116a4c02f85f6556cdaf7228
booldook <booldook@gmail.com>
2020-11-17 11:25:22 +0900

    pager완성

--- modules/pager-conn.js
+++ modules/pager-conn.js
@@ -1,13 +1,14 @@
 const pager = (page, totalRecord, obj) => {
+	page = Number(page);
 	let { listCnt = 5, pagerCnt = 3 } = obj || {};
 	let totalPage = Math.ceil(totalRecord / listCnt);
 	let startIdx = (page - 1) * listCnt;
-	let lastPage = totalPage;
+	let lastPage = page < totalPage ? totalPage : 0;
 	let startPage = Math.floor((page - 1) / listCnt) * pagerCnt + 1;
-	let endPage = startPage + pagerCnt - 1 > lastPage ? lastPage : startPage + pagerCnt - 1;
-	let nextPage = page + 1 > lastPage ? 0 : page + 1;
+	let endPage = startPage + pagerCnt - 1 > totalPage ? totalPage : startPage + pagerCnt - 1;
+	let nextPage = page + 1 > totalPage ? 0 : page + 1;
 	let prevPage = page - 1;
-	let nextPager = endPage + 1 > lastPage ? 0 : endPage + 1;
+	let nextPager = endPage + 1 > totalPage ? 0 : endPage + 1;
 	let prevPager = startPage - 1;
 	let firstPage = 1;
 	return { page, totalRecord, listCnt, pagerCnt, totalPage, startIdx, startPage, endPage, nextPage, prevPage, nextPager, prevPager, firstPage, lastPage };
